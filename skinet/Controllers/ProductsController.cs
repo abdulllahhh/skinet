2fa2a7f7b565061c6f67362c75ba2c4b8db58520
abdullah <56201506+abdulllahhh@users.noreply.github.com>
2025-02-11 06:36:27 +0200

    initial commit

---
+++ skinet/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿
+using core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace skinet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>> >GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id )
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
